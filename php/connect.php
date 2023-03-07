09ee63094bc652dbb898f93ef98803dda9a93d8f
Jean-Maxime Dereuder <101884584+Maxime45470@users.noreply.github.com>
2023-03-07 21:18:44 +0100

    mise a jour

--- php/connect.php
+++ php/connect.php
@@ -14,17 +14,14 @@ function pdo_connect(){
 }
 */
 ?>
-
 <?php
-
-
-function pdo_connect(){
+function pdo_connect()
+{
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
     $DATABASE_PASS = 'root';
     $DATABASE_NAME = 'db_score';
-
-    try{
+    try {
         return new PDO('mysql:host='.$DATABASE_HOST. ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch(PDOException $exception) {
         exit('failed to connect bdd');
